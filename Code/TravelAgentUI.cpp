e99882bd0dc6e3dc42c1eb3b0e6a8c60ea7f3616
PeterMav <peter.mavo@gmail.com>
2015-05-21 20:25:22 +1000

    Added only a few system calls
    I don’t want to add more right now for clearing the screen because I
    believe that it won’t work on Windows.

--- Code/TravelAgentUI.cpp
+++ Code/TravelAgentUI.cpp
@@ -10,37 +10,39 @@ TravelAgentUI::TravelAgentUI(sqlite3* d) {
 }
 
 bool TravelAgentUI::run() {
+    
     string input = "";
 	
 	while(input != "0"){
-			cout<<"\t\t\tTRAVEL AGENT HOME"<<endl;
-			cout<<"please select an option: "<<endl;
-			cout<<"1) Book flight for a customer. "<<endl;
-			cout<<"2) View all bookings made."<<endl;
-			cout<<"3) Add new customer."<<endl;
-			cout<<"4) Edit own details."<<endl;
-			cout<<"0) Log Out."<<endl;
-			cout<<"Your choice: ";
-			cin>>input;
+        system("clear");
+        cout << "\t\t\tTRAVEL AGENT HOME" << endl;
+        cout << "please select an option: " << endl;
+        cout << "1) Book flight for a customer. " << endl;
+        cout << "2) View all bookings made." << endl;
+        cout << "3) Add new customer." << endl;
+        cout << "4) Edit own details." << endl;
+        cout << "0) Log Out." << endl;
+        cout << "Your choice: ";
+        cin >> input;
 			
-			if(input == "1"){
-				BookingManagerUI bmUI(db);
-				input = bmUI.run();
-				//bookFlights();
-			}
-			else if(input == "2"){
-				bookingsMade();
-			}
-			else if(input == "3"){
-                CustomerProfileController cpc(db);
-                cpc.createCustomer();
-                //addCustomer();
-			}
-			else if(input == "4"){
-				editDetails();
-			}
+        if(input == "1"){
+            BookingManagerUI bmUI(db);
+            input = bmUI.run();
+            //bookFlights();
+        }
+        else if(input == "2"){
+            bookingsMade();
+        }
+        else if(input == "3"){
+            CustomerProfileController cpc(db);
+            cpc.createCustomer();
+            //addCustomer();
+        }
+        else if(input == "4"){
+            editDetails();
+        }
 	}
-
+    system("clear");
     return 1;
 }
 
