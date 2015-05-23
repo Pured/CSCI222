094bb4dc43e425579e627f38601ffcf01d477ec7
Pured <krajl_kresimir@hotmail.com>
2015-05-23 13:44:14 +1000

    Restyled CustomerProfileController.

--- Code/CustomerProfileController.h
+++ Code/CustomerProfileController.h
@@ -1,26 +1,28 @@
-#ifndef CUSTOMERPROFILECONTROLLER_H
-#define CUSTOMERPROFILECONTROLLER_H
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
+
+#ifndef CUSTOMERPROFILECONTROLLER_H_
+#define CUSTOMERPROFILECONTROLLER_H_
 
 #include <string>
-#include <iostream>
 #include "sqlite3.h"
 
-class CustomerProfileController
-{
-    private:
-		sqlite3* db;
+class CustomerProfileController{
+private:
+	sqlite3 *db;
 
-    public:
-        //constructors
-        CustomerProfileController(sqlite3* d);
-    
-		//functions
-		std::string registerExistingCustomer(std::string,std::string);
-		void findCustomer();
-		void createCustomer();
-		void editCustomer(std::string,std::string);
-		void deleteCustomer();
+public:
+	CustomerProfileController(sqlite3 *d); // Constructor.
 
+	// Functions.
+	std::string registerExistingCustomer(std::string, std::string);
+	void findCustomer();
+	void createCustomer();
+	void editCustomer(std::string, std::string);
+	void deleteCustomer();
 };
 
-#endif // CUSTOMERPROFILECONTROLLER_H
+#endif // CUSTOMERPROFILECONTROLLER_H_
