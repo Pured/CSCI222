98daf0050c78f357801d0b41d7d829734a17660a
Pured <krajl_kresimir@hotmail.com>
2015-05-26 19:37:36 +1000

    Restyled Main.

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,25 +1,20 @@
 /*=============================================================
 | Modified by: kb100
-| Version: 1.00
-| Description: The main file.
+| Version: 1.02
+| Modification: Restyled the code.
 |==============================================================*/
-
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "sqlite3.h"
 #include "GuestUI.h"
-
 #include "Booking.h"
 #include "Customer.h"
 #include "CustomerProfileController.h"
-
-
-
 #include "Seat.h"
 #include "Aircraft.h"
 #include "BookingController.h"
+#include "sqlite3.h"
 
 using namespace std;
 
@@ -27,28 +22,26 @@ int main(int argc, char const *argv[])
 {
     system("clear");
 	
-	//Open FlightDB database
+	// Open FlightDB database.
 	sqlite3 *db;
 	char *errMsg = 0;
 	int err = sqlite3_open("FlightDB.db", &db); 
 
 	if(err != SQLITE_OK){
-		cout<<"Cannot access database"<<endl;
+		cout << "Cannot access database.\n";
 	}
 	else{
-		cout<<"Connected to database."<<endl;
+		cout << "Connected to database.\n";
 	}
-	
-	
+
 	GuestUI gUI(db);
 	bool end = false;
 	
 	//main loop
-	while (end == false){
+	while(end == false){
 		end = gUI.run();
 	}
-	
-	
+
 	/*
 	BookingController BC(db);
 	Schedule S(db);
@@ -57,8 +50,7 @@ int main(int argc, char const *argv[])
 	//BC.displaySeating("Premium Economy",S);
 	*/
 
+	sqlite3_close(db); // Close FlightDB database.
 
-	//close FlightDB database.
-	sqlite3_close(db);
 	return 0;
 }
