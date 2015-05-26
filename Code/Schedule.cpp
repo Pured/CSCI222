08ff0eca65a0f4874b498bf8569a61c8b677379d
Pured <krajl_kresimir@hotmail.com>
2015-05-26 22:30:26 +1000

    Restyled Schedule.

--- Code/Schedule.cpp
+++ Code/Schedule.cpp
@@ -1,35 +1,37 @@
-//
-//  Schedule.cpp
-//  222test
-//
-//
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#include "Schedule.h"
+#include <iostream>
 #include <sstream>
+#include "Schedule.h"
 #include "callback.h"
-#include "sqlite3.h"
 
-//constructors
-Schedule::Schedule(sqlite3* d){
-	db=d;
-    ID = -1;
-    flightID = "No FlightID";
-    plane = -1;
-    route = -1;
+using namespace std;
+
+// Constructors.
+Schedule::Schedule(sqlite3 *d){
+	db = d;
+	ID = -1;
+	flightID = "No FlightID";
+	plane = -1;
+	route = -1;
 	departDay = "No departure day";
 	depart = "No Departure";
-	departTimezone = "no depart timezone";
-	arriveDay = "no arrival Day";
+	departTimezone = "No depart timezone";
+	arriveDay = "No arrival Day";
 	arrive = "No Arrive";
 	arriveTimezone = "No arrival timezone";
 }
 
 Schedule::Schedule(){
 	db = NULL;
-	 ID = -1;
-    flightID = "No FlightID";
-    plane = -1;
-    route = -1;
+	ID = -1;
+	flightID = "No FlightID";
+	plane = -1;
+	route = -1;
 	departDay = "No departure day";
 	depart = "No Departure";
 	departTimezone = "no depart timezone";
@@ -38,12 +40,12 @@ Schedule::Schedule(){
 	arriveTimezone = "No arrival timezone";
 }
 
-//get functions
+// Get functions.
 int Schedule::getID() const{
     return ID;
 }
 
-std::string Schedule::getFlightID() const{
+string Schedule::getFlightID() const{
     return flightID;
 }
 
@@ -55,119 +57,113 @@ int Schedule::getRoute() const{
     return route;
 }
 
-std::string Schedule::getDepartDay() const{
+string Schedule::getDepartDay() const{
     return departDay;
 }
 
-std::string Schedule::getDepart() const{
+string Schedule::getDepart() const{
     return depart;
 }
 
-std::string Schedule::getDepartTimezone() const{
+string Schedule::getDepartTimezone() const{
     return departTimezone;
 }
 
-std::string Schedule::getArriveDay() const{
+string Schedule::getArriveDay() const{
     return arriveDay;
 }
 
-std::string Schedule::getArrive() const{
+string Schedule::getArrive() const{
     return arrive;
 }
 
-std::string Schedule::getArriveTimezone() const{
+string Schedule::getArriveTimezone() const{
     return arriveTimezone;
 }
 
-std::string Schedule::getByID(int n){
-	std::stringstream convert;
+string Schedule::getByID(int n){
+	stringstream convert;
+
 	convert << n;
-	std::string convID = convert.str();
-	convert.str(std::string()); //clear ss
+
+	string convID = convert.str();
+	convert.str(string()); // Clear ss.
 	
-	std::string sqlCreate = "SELECT  * FROM SCHEDULE WHERE ID = '" + convID +"';";
-	const char* sql = sqlCreate.c_str();
+	string sqlCreate = "SELECT * FROM SCHEDULE WHERE ID = '" + convID + "';";
+	const char *sql = sqlCreate.c_str();
 	
 	sqlite3_stmt *stmt;
-    int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+	int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
 	
-	int FID = -1;
-	const char* FLIGHTID;
-	int PLANE;
-	int ROUTE;
-	const char * DEPARTDAY;
-	const char* DEPART;
-	const char * DEPARTTZ;
-	const char * ARRIVEDAY;
-	const char* ARRIVE;
-	const char * ARRIVETZ;
+	int FID = -1, PLANE = 0, ROUTE = 0;
+	const char *FLIGHTID, *DEPARTDAY, *DEPART, *DEPARTTZ, *ARRIVEDAY, *ARRIVE, *ARRIVETZ;
 	
-	if (err != SQLITE_OK) {
-        std::cout << "SELECT failed: " << sqlite3_errmsg(db) << std::endl;
+	if(err != SQLITE_OK){
+        cout << "SELECT failed: " << sqlite3_errmsg(db) << endl;
     }
     else{
-		while (sqlite3_step(stmt) == SQLITE_ROW ) {
+		while(sqlite3_step(stmt) == SQLITE_ROW){
 			FID = sqlite3_column_int(stmt, 0);
-            FLIGHTID = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)); //get col 0
+
+            FLIGHTID = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
+
 			PLANE = sqlite3_column_int(stmt, 2);
 			ROUTE = sqlite3_column_int(stmt, 3);
-			DEPARTDAY = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4)); //get col 0
-			DEPART = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 5)); //get col 0
-			DEPARTTZ = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 6)); //get col 0
-			ARRIVEDAY = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 7)); //get col 0
-			ARRIVE = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 8)); //get col 0
-			ARRIVETZ = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 9)); //get col 0
-			
-			//std::cout<<FID<<" "<<FLIGHTID<<" "<<PLANE<<" "<<ROUTE<<" "<<DEPARTDAY<<DEPART<<DEPARTTZ
-			//<<" "<<ARRIVEDAY<<ARRIVE<<ARRIVETZ<< "\n";
-			
+
+			DEPARTDAY = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 4));
+			DEPART = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 5));
+			DEPARTTZ = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 6));
+			ARRIVEDAY = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 7));
+			ARRIVE = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 8));
+			ARRIVETZ = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 9));
+
 			if(FLIGHTID == NULL){
 				flightID = "";
 			}
 			else{
-				flightID= std::string(FLIGHTID);
+				flightID = string(FLIGHTID);
 			}
-			
+
 			if(DEPARTDAY == NULL){
 				departDay = "";
 			}
 			else{
-				departDay = std::string(DEPARTDAY);
+				departDay = string(DEPARTDAY);
 			}
-			
+
 			if(DEPART == NULL){
 				depart = "";
 			}
 			else{
-				depart = std::string(DEPART);
+				depart = string(DEPART);
 			}
-			
+
 			if(DEPARTTZ == NULL){
-				departTimezone= "";
+				departTimezone = "";
 			}
 			else{
-				departTimezone = std::string(DEPARTTZ);
+				departTimezone = string(DEPARTTZ);
 			}
-			
+
 			if(ARRIVEDAY == NULL){
 				arriveDay = "";
 			}
 			else{
-				arriveDay = std::string(ARRIVEDAY);
+				arriveDay = string(ARRIVEDAY);
 			}
-			
+
 			if(ARRIVE == NULL){
 				arrive = "";
 			}
 			else{
-				arrive = std::string(ARRIVE);
+				arrive = string(ARRIVE);
 			}
-			
+
 			if(ARRIVETZ == NULL){
 				arriveTimezone = "";
 			}
 			else{
-				arriveTimezone = std::string(ARRIVETZ);
+				arriveTimezone = string(ARRIVETZ);
 			}
 			
 			ID = FID;
@@ -175,111 +171,105 @@ std::string Schedule::getByID(int n){
 			route = ROUTE;
 		}
 	}
-	
+
 	sqlite3_finalize(stmt);
-	
+
 	if(ID == -1){
 		return "NOT FOUND";
 	}
-	
+
 	return "FOUND";
 }
 
-//set functions
-void Schedule::setDB(sqlite3* d){
-    db=d;
+// Set functions.
+void Schedule::setDB(sqlite3 *d){
+	db = d;
 }
 
 void Schedule::setID(int i){
-    ID = i;
+	ID = i;
 }
 
-void Schedule::setFlightID(std::string i){
-    flightID = i;
+void Schedule::setFlightID(string i){
+	flightID = i;
 }
 
 void Schedule::setPlane(int i){
-    plane = i;
+	plane = i;
 }
 
 void Schedule::setRoute(int i){
-    route = i;
+	route = i;
 }
 
-void Schedule::setDepart(std::string i){
-    depart = i;
+void Schedule::setDepart(string i){
+	depart = i;
 }
 
-void Schedule::setDepartDay(std::string i){
-    departDay = i;
+void Schedule::setDepartDay(string i){
+	departDay = i;
 }
 
-void Schedule::setDepartTimezone(std::string i){
-    departTimezone = i;
+void Schedule::setDepartTimezone(string i){
+	departTimezone = i;
 }
 
-void Schedule::setArriveDay(std::string i){
-    arriveDay = i;
+void Schedule::setArriveDay(string i){
+	arriveDay = i;
 }
 
-void Schedule::setArrive(std::string i){
-    arrive = i;
+void Schedule::setArrive(string i){
+	arrive = i;
 }
 
-void Schedule::setArriveTimezone(std::string i){
-    arriveTimezone = i;
+void Schedule::setArriveTimezone(string i){
+	arriveTimezone = i;
 }
-
 
 int Schedule::update(){
-		
 	if(ID != -1){
-		//convert any numeric attributes to string
-		std::stringstream convert;
+		// Convert any numeric attributes to string.
+		stringstream convert;
+
 		convert << ID;
-		std::string convID = convert.str();
-		convert.str(std::string()); //clear s
+
+		string convID = convert.str();
+		convert.str(string()); // Clear ss.
 		
 		convert << plane;
-		std::string convPlane = convert.str();
-		convert.str(std::string()); //clear s
+		string convPlane = convert.str();
+		convert.str(string()); // Clear ss.
 		
 		convert << route;
-		std::string convRoute = convert.str();
-		convert.str(std::string()); //clear s
+		string convRoute = convert.str();
+		convert.str(string()); // Clear ss.
 	
-		std::string createSql = "UPDATE SCHEDULE SET ID = '" + convID + "' WHERE ID = '"+ convID + "'; " 
-							+ 	"UPDATE SCHEDULE SET FLIGHTID = '" + flightID + "' WHERE ID = '"+ convID + "'; " 
-							+ 	"UPDATE SCHEDULE SET PLANE = '" + convPlane + "' WHERE ID = '"+ convID + "'; " 
-							+ 	"UPDATE SCHEDULE SET ROUTE = '" + convRoute + "' WHERE ID = '"+ convID + "'; "
-							+ 	"UPDATE SCHEDULE SET DEPART = '" + depart + "' WHERE ID = '"+ convID + "'; " 
-							+ 	"UPDATE SCHEDULE SET ARRIVE = '" + arrive + "' WHERE ID = '"+ convID + "'; " ;
+		string createSql = "UPDATE SCHEDULE SET ID = '" + convID + "' WHERE ID = '" + convID + "';" + "UPDATE SCHEDULE SET FLIGHTID = '" + flightID + "' WHERE ID = '" + convID + "';" + "UPDATE SCHEDULE SET PLANE = '" + convPlane + "' WHERE ID = '" + convID + "';" + "UPDATE SCHEDULE SET ROUTE = '" + convRoute + "' WHERE ID = '" + convID + "';" + "UPDATE SCHEDULE SET DEPART = '" + depart + "' WHERE ID = '" + convID + "';" + "UPDATE SCHEDULE SET ARRIVE = '" + arrive + "' WHERE ID = '" + convID + "';"
 
-							
-							//std::cout<<createSql<<std::endl;
-		const char* sql = createSql.c_str();
-	
-		/* Execute SQL statement */
-   
-		char* errMsg = 0;
+		const char *sql = createSql.c_str();
+
+		// Execute SQL statement.
+		char *errMsg = 0;
 		int err = sqlite3_exec(db, sql, callback, 0, &errMsg);
-		if( err != SQLITE_OK ){
-			std::cout<<"SQL error: "<<errMsg<<std::endl;
+
+		if(err != SQLITE_OK){
+			cout << "SQL error: " << errMsg << endl;
+
 			return 1;
 		}
 	}
 	else{
-		std::cout<<"Schedule  not initialised in UPDATE."<<std::endl;
+		cout << "Schedule not initialised in UPDATE.\n";
+
 		return 1;
 	}
 
    return 0;
 }
 
-//other functions
-std::ostream &operator<<( std::ostream &os, const Schedule &S){
-    os<<"Schedule ID: "<<S.getID()<<"\nFlightID: "<<S.getFlightID()<<"\nPlane: "<<S.getPlane()<<"\nRoute: "<<S.getRoute()<<" "
-	<<"\nDeparture Day: "<<S.getDepartDay()<<"\nDeparture Date: "<<S.getDepart()<<"\nDeparture Timezone: "<<S.getDepartTimezone()
-	<<"\nArrival Day: "<<S.getArriveDay()<<"\nArrival Date: "<<S.getArrive()<<"\nArrival Timezone: "<<S.getArriveTimezone();
-    return os;
+// Other functions.
+ostream &operator<<(ostream &os, const Schedule &S){
+	os << "Schedule ID: " << S.getID() << "\nFlightID: " << S.getFlightID() << "\nPlane: " << S.getPlane() << "\nRoute: " << S.getRoute() << " " << "\nDeparture Day: " << S.getDepartDay() << "\nDeparture Date: " << S.getDepart() << "\nDeparture Timezone: " << S.getDepartTimezone() << "\nArrival Day: " << S.getArriveDay() << "\nArrival Date: " << S.getArrive() << "\nArrival Timezone: " << S.getArriveTimezone();
+
+	return os;
 }
--- Code/Schedule.h
+++ Code/Schedule.h
@@ -1,39 +1,25 @@
-//
-//  Schedule.h
-//  FlightSystem
-//
-//
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#ifndef ___FlightSystem__Schedule__
-#define ___FlightSystem__Schedule__
+#ifndef SCHEDULE_H_
+#define SCHEDULE_H_
 
-#include <iostream>
 #include "sqlite3.h"
 
 class Schedule{
-private:
-	sqlite3* db;
-    int ID;
-    std::string flightID;
-    int plane;
-    int route;
-	std::string departDay; //day of week of departure
-	std::string depart; //date of departure
-	std::string departTimezone; //timezone of departing airport
-	std::string arriveDay; //day of week of arrival
-	std::string arrive; //date of arrival
-	std::string arriveTimezone; //timezone of departing airport
-    
 public:
-    //constructors
-    Schedule(sqlite3* d);
+	// Constructors.
+	Schedule(sqlite3 *d);
 	Schedule();
-    
-    //get functions
-    int getID() const;
-    std::string getFlightID() const;
-    int getPlane() const;
-    int getRoute() const;
+
+	// Get functions.
+	int getID() const;
+	std::string getFlightID() const;
+	int getPlane() const;
+	int getRoute() const;
 	std::string getDepartDay() const;
 	std::string getDepart() const;
 	std::string getDepartTimezone() const;
@@ -42,12 +28,12 @@ public:
 	std::string getArriveTimezone() const;
 	std::string getByID(int);
 
-    //set functions
-	void setDB(sqlite3*);
-    void setID(int);
-    void setFlightID(std::string);
-    void setPlane(int);
-    void setRoute(int);
+	// Set functions.
+	void setDB(sqlite3 *);
+	void setID(int);
+	void setFlightID(std::string);
+	void setPlane(int);
+	void setRoute(int);
 	void setDepartDay(std::string);
 	void setDepart(std::string);
 	void setDepartTimezone(std::string);
@@ -56,9 +42,21 @@ public:
 	void setArriveTimezone(std::string);
 	int update();
 
-    //other functions
-    friend std::ostream &operator<<( std::ostream &output, const Schedule &T);
+    // Other functions.
+    friend std::ostream &operator<<(std::ostream &output, const Schedule &T);
 
+private:
+	sqlite3 *db;
+	int ID;
+	std::string flightID;
+	int plane;
+	int route;
+	std::string departDay; // Day of week of departure.
+	std::string depart; // Date of departure.
+	std::string departTimezone; // Timezone of departing airport.
+	std::string arriveDay; // Day of week of arrival.
+	std::string arrive; // Date of arrival.
+	std::string arriveTimezone; // Timezone of departing airport.
 };
 
-#endif /* defined(___FlightSystem__Schedule__) */
+#endif // SCHEDULE_H_
