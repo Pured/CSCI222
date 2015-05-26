5f5f81f27c10b376babf748c6d085e7ba186604c
Pured <krajl_kresimir@hotmail.com>
2015-05-27 00:20:09 +1000

    Restyled Staff.

--- Code/Staff.cpp
+++ Code/Staff.cpp
@@ -1,109 +1,100 @@
+/*=============================================================
+| Modified by: kb100
+| Version: 1.02
+| Modification: Restyled the code.
+|==============================================================*/
 
+#include <iostream>
 #include "Staff.h"
-#include "sqlite3.h"
 
-Staff::Staff(sqlite3* d){
+using namespace std;
+
+// Constructors.
+Staff::Staff(sqlite3 *d){
 	db = d;
-    userType = "None";
-    userName = "No Username";
-    password = "No Password";
+	userType = "None";
+	userName = "No Username";
+	password = "No Password";
 }
 
-Staff::Staff(sqlite3* d,std::string uType,std::string uName, std::string pw){
-	db=d;
-    userType = uType;
-    userName = uName;
-    password = pw;
-    
+Staff::Staff(sqlite3 *d, string uType, string uName, string pw){
+	db = d;
+	userType = uType;
+	userName = uName;
+	password = pw;
 }
 
-
-
-
-//set functions
-void Staff::setUsername(std::string i){
-    userName = i;
+// Set functions.
+void Staff::setUsername(string i){
+	userName = i;
 }
 
-void Staff::setPassword(std::string i){
-    password = i;
+void Staff::setPassword(string i){
+	password = i;
 }
 
-void Staff::setType(std::string i){
-    userType = i;
+void Staff::setType(string i){
+	userType = i;
 }
 
-//sets all staff member attributes by finding their email address from the database.
-std::string Staff::setByEmail(std::string e){
-	
-	std::string sqlCreate = "SELECT  USERTYPE,EMAIL,PASSWORD FROM STAFF WHERE EMAIL = '" + e +"';";
-	const char* sql = sqlCreate.c_str();
-	
-	//std::cout<<sql<<std::endl;
+// Sets all staff member attributes by finding their email address from the database.
+string Staff::setByEmail(string e){
+	string sqlCreate = "SELECT USERTYPE, EMAIL, PASSWORD FROM STAFF WHERE EMAIL = '" + e + "';";
+	const char *sql = sqlCreate.c_str();
 
 	sqlite3_stmt *stmt;
-    int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+	int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
 
-    //create variables to store data.
-    const char* UT; //temp storage for userType
-    const char* UN; //temp storage for userNAme;
-    const char* PWD; //temp storage for password
+	// Create variables to store data.
+	const char* UT; // Temp storage for userType.
+	const char* UN; // Temp storage for userNAme.
+	const char* PWD; // Temp storage for password.
 
-    if (err != SQLITE_OK) {
-        std::cout << "SELECT failed: " << sqlite3_errmsg(db) << std::endl;
-    }
-    else{
-		while (sqlite3_step(stmt) == SQLITE_ROW) {
-			//get data from db
-            UT = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)); //get col 0
-            UN= reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)); // get col 1
-            PWD = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)); // get col 2
-			
-			//std::cout<<UT<<" "<<UN<<" "<<PWD;
-			
-			//cast to string
-			if(UT == NULL || UN == NULL || PWD == NULL){ //make sure UT/UN/PWD is not null, otherwise the return behaviour is undefined.
+	if(err != SQLITE_OK){
+		cout << "SELECT failed: " << sqlite3_errmsg(db) << endl;
+	}
+	else{
+		while(sqlite3_step(stmt) == SQLITE_ROW){
+			// Get data from db.
+			UT = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)); // Get column 0.
+			UN = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1)); // Get column 1.
+			PWD = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2)); // Get column 2.
+
+			// Cast to string.
+			if(UT == NULL || UN == NULL || PWD == NULL){ // Make sure UT/UN/PWD is not null, otherwise the return behaviour is undefined.
 				userType = "";
 				userName = "";
 				password = "";
 			}
 			else{
-				userType = std::string(UT);
-				userName = std::string(UN);
-				password = std::string(PWD);
+				userType = string(UT);
+				userName = string(UN);
+				password = string(PWD);
 			}
-			
-        }
-    }
+		}
+	}
 
-    sqlite3_finalize(stmt);
-	
+	sqlite3_finalize(stmt);
+
 	return userType;
 };
 
-
-
-
-
-//get functions
-std::string Staff::getPassword() const{
-    return password;
+// Get functions.
+string Staff::getPassword() const{
+	return password;
 }
 
-std::string Staff::getType() const{
-    return userType;
+string Staff::getType() const{
+	return userType;
 }
 
-std::string Staff::getUsername() const{
-    return userName;
+string Staff::getUsername() const{
+	return userName;
 }
 
+// Other functions.
+ostream &operator<<(ostream &output, const Staff &S){
+	output << S.getUsername() << " " << S.getPassword();
 
-
-
-
-//other functions
-std::ostream &operator<<( std::ostream &output,const Staff &S){
-    output<<S.getUsername()<<" "<<S.getPassword();
-    return output;
+	return output;
 }
--- Code/Staff.h
+++ Code/Staff.h
@@ -1,37 +1,40 @@
-#ifndef STAFF_H
-#define STAFF_H
+/*=============================================================
+| Modified by: kb100
+| Version: 1.02
+| Modification: Restyled the code.
+|==============================================================*/
+
+#ifndef STAFF_H_
+#define STAFF_H_
 
 #include <string>
-#include <iostream>
 #include "sqlite3.h"
 
-class Staff
-{
-    private:
-		sqlite3* db;
-        std::string userType;
-        std::string userName;
-        std::string password;
-    
-    public:
-        //constructors
-        Staff(sqlite3* d);
-        Staff(sqlite3*,std::string uType,std::string uName, std::string pw);
-    
-        //get functions
-        std::string getUsername() const;
-        std::string getPassword() const;
-        std::string getType() const;
-    
-        //set functions
-        void setUsername(std::string);
-        void setPassword(std::string);
-        void setType(std::string);
-		std::string setByEmail(std::string e);
-    
-        //other functions
-        friend std::ostream &operator<<( std::ostream &output,const Staff &S);
+class Staff{
+public:
+	// Constructors.
+	Staff(sqlite3 *d);
+	Staff(sqlite3 *, std::string uType, std::string uName, std::string pw);
 
+	// Get functions.
+	std::string getUsername() const;
+	std::string getPassword() const;
+	std::string getType() const;
+
+	// Set functions.
+	void setUsername(std::string);
+	void setPassword(std::string);
+	void setType(std::string);
+	std::string setByEmail(std::string e);
+
+	// Other functions.
+	friend std::ostream &operator<<(std::ostream &output, const Staff &S);
+
+private:
+	sqlite3 *db;
+	std::string userType;
+	std::string userName;
+	std::string password;
 };
 
-#endif // STAFF_H
+#endif // STAFF_H_
