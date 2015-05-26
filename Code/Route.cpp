3626e69daee24653298f84280a360b8f3cc11adc
Pured <krajl_kresimir@hotmail.com>
2015-05-26 20:23:56 +1000

    Restyled Route.

--- Code/Route.cpp
+++ Code/Route.cpp
@@ -1,249 +1,238 @@
-//
-//  Route.cpp
-//  222test
-//
-//  Created by Darryl Murphy on 5/04/15.
-//  Copyright (c) 2015 Darryl Murphy. All rights reserved.
-//
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#include "Route.h"
-#include "sqlite3.h"
-#include "callback.h"
-#include <sstream>
 #include <iostream>
+#include <sstream>
+#include "Route.h"
+#include "callback.h"
+#include "sqlite3.h"
 
 using namespace std;
 
-//Constructors
-
-Route::Route(sqlite3* d){
+// Constructors.
+Route::Route(sqlite3 *d){
 	db = d;
-    ID = -1;
-    srcAirport = "No Source Airport";
-    destAirport = "No Destination Airport";
-    codeshare = "No Codeshare";
-    stops = 999;
+	ID = -1;
+	srcAirport = "No Source Airport";
+	destAirport = "No Destination Airport";
+	codeshare = "No Codeshare";
+	stops = 999;
 }
 
 Route::Route(){
 	db = NULL;
 	ID = -1;
-    srcAirport = "No Source Airport";
-    destAirport = "No Destination Airport";
-    codeshare = "No Codeshare";
-    stops = 999;
+	srcAirport = "No Source Airport";
+	destAirport = "No Destination Airport";
+	codeshare = "No Codeshare";
+	stops = 999;
 }
 
-
-
-//get functions
+// Get functions.
 int Route::getID() const{
-    return ID;
+	return ID;
 }
 
-std::string Route::getSrc() const{
-    return srcAirport;
+string Route::getSrc() const{
+	return srcAirport;
 }
 
-std::string Route::getDest() const{
-    return destAirport;
+string Route::getDest() const{
+	return destAirport;
 }
 
-std::string Route::getCodeshare() const{
-    return codeshare;
+string Route::getCodeshare() const{
+	return codeshare;
 }
 
 int Route::getStops() const{
-    return stops;
+	return stops;
 }
 
-std::string Route::getByID(int id){
-	std::stringstream convert;
+string Route::getByID(int id){
+	stringstream convert;
+
 	convert << id;
-	std::string convid = convert.str();
-	convert.str(std::string()); //clear ss
+
+	string convid = convert.str();
+	convert.str(string()); // Clear ss.
 	
-	std::string sqlCreate = "SELECT  * FROM ROUTE WHERE ID = '" + convid +"';";
-	const char* sql = sqlCreate.c_str();
+	string sqlCreate = "SELECT  * FROM ROUTE WHERE ID = '" + convid + "';";
+	const char *sql = sqlCreate.c_str();
 	
 	sqlite3_stmt *stmt;
-    int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+	int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
 	
-	int ROUTEID = -1 ,STOPS;
-	const char* SRC;
-	const char* DEST;
-	const char* CODE;
+	int ROUTEID = -1 , STOPS = 0;
+	const char *SRC, *DEST, *CODE;
 	
-		 if (err != SQLITE_OK) {
-        std::cout << "SELECT failed: " << sqlite3_errmsg(db) << std::endl;
-    }
-    else{
-		while (sqlite3_step(stmt) == SQLITE_ROW ) {
+	if(err != SQLITE_OK){
+		cout << "SELECT failed: " << sqlite3_errmsg(db) << endl;
+	}
+	else{
+		while(sqlite3_step(stmt) == SQLITE_ROW){
 			ROUTEID = sqlite3_column_int(stmt, 0);
-            SRC = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)); //get col 0
-			DEST= reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)); // get col 1
-            CODE= reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3)); // get col 1
+
+			SRC = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
+			DEST = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2));
+			CODE = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3));
+
 			STOPS = sqlite3_column_int(stmt, 4);
-			
-			//std::cout<<ROUTEID<<" "<<SRC<<" "<<DEST<<" "<<CODE<<" "<<STOPS<<"\n";
-			
+
 			if(SRC == NULL){
 				srcAirport = "";
 			}
 			else{
-				srcAirport = std::string(SRC);
+				srcAirport = string(SRC);
 			}
-			
+
 			if(DEST == NULL){
 				destAirport = "";
 			}
 			else{
-				destAirport = std::string(DEST);
+				destAirport = string(DEST);
 			}
-			
+
 			if(CODE == NULL){
 				codeshare = "";
 			}
 			else{
-				codeshare = std::string(CODE);
+				codeshare = string(CODE);
 			}
-			
+
 			ID = ROUTEID;
 			stops = STOPS;
 		}
 	}
-	
+
 	sqlite3_finalize(stmt);
 	
 	if(ID == -1){
 		return "NOT FOUND";
 	}
-	
+
 	return "FOUND";
 }
 
-
-//set Functions
+// Set Functions.
 void Route::setID(int i){
-    ID = i;
+	ID = i;
 }
 
-void Route::setSrc(std::string i){
-    srcAirport = i;
+void Route::setSrc(string i){
+	srcAirport = i;
 }
 
-void Route::setDest(std::string i){
-    destAirport = i;
+void Route::setDest(string i){
+	destAirport = i;
 }
 
-void Route::setCodeshare(std::string i){
-    codeshare = i;
+void Route::setCodeshare(string i){
+	codeshare = i;
 }
 
 void Route::setStops(int i){
-    stops = i;
+	stops = i;
 }
 
 int Route::update(){
-	
 	if(ID != -1){
-		//convert any numeric attributes to string
-		std::stringstream convert;
+		// Convert any numeric attributes to string.
+		stringstream convert;
+
 		convert << ID;
-		std::string convID = convert.str();
-		convert.str(std::string()); //clear ss
+
+		string convID = convert.str();
+		convert.str(string()); // Clear ss.
 	
 		convert << stops;
-		std::string convStops = convert.str();
-		convert.str(std::string()); //clear ss
-	
-	
-		std::string createSql = "UPDATE ROUTE SET ID = '" + convID + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE ROUTE SET SRC = '" + srcAirport + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE ROUTE SET DEST = '" + destAirport + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE ROUTE SET CODESHARE = '" + codeshare + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE ROUTE SET STOPS = '" + convStops + "' WHERE ID = "+ convID + "; ";
 
-							
-							//std::cout<<createSql<<std::endl;
-		const char* sql = createSql.c_str();
-	
-		/* Execute SQL statement */
-   
-		char* errMsg = 0;
+		string convStops = convert.str();
+		convert.str(string()); // Clear ss.
+
+		string createSql = "UPDATE ROUTE SET ID = '" + convID + "' WHERE ID = " + convID + ";" + "UPDATE ROUTE SET SRC = '" + srcAirport + "' WHERE ID = "+ convID + ";" + "UPDATE ROUTE SET DEST = '" + destAirport + "' WHERE ID = " + convID + ";" + "UPDATE ROUTE SET CODESHARE = '" + codeshare + "' WHERE ID = " + convID + ";" + "UPDATE ROUTE SET STOPS = '" + convStops + "' WHERE ID = " + convID + ";";
+
+		const char *sql = createSql.c_str();
+
+		// Execute SQL statement.
+		char *errMsg = 0;
 		int err = sqlite3_exec(db, sql, callback, 0, &errMsg);
-		if( err != SQLITE_OK ){
-			std::cout<<"SQL error: "<<errMsg<<std::endl;
+
+		if(err != SQLITE_OK){
+			cout << "SQL error: " << errMsg << endl;
+
 			return 1;
 		}
 	}
 	else{
-		std::cout<<"ROUTE  not initialised in UPDATE."<<std::endl;
+		cout << "ROUTE  not initialised in UPDATE.\n";
+
 		return 1;
 	}
 
-   return 0;
+	return 0;
 }
 
-int Route::getByAirports(std::string leaving, std::string arriving){
-	
-	std::string sqlCreate = "SELECT  * FROM ROUTE WHERE SRC='" + leaving + "' AND DEST='"+ arriving +"';";
-	//cout<<sqlCreate<<endl;
-	const char* sql = sqlCreate.c_str();
+int Route::getByAirports(string leaving, string arriving){
+	string sqlCreate = "SELECT  * FROM ROUTE WHERE SRC='" + leaving + "' AND DEST='" + arriving + "';";
+
+	const char *sql = sqlCreate.c_str();
 	
 	sqlite3_stmt *stmt;
     int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
 	
-	int ROUTEID = -1 ,STOPS;
-	const char* SRC;
-	const char* DEST;
-	const char* CODE;
+	int ROUTEID = -1 , STOPS = 0;
+	const char* SRC, *DEST, *CODE;
 	
-		 if (err != SQLITE_OK) {
-        std::cout << "SELECT failed: " << sqlite3_errmsg(db) << std::endl;
+	if(err != SQLITE_OK){
+        cout << "SELECT failed: " << sqlite3_errmsg(db) << endl;
     }
     else{
-		while (sqlite3_step(stmt) == SQLITE_ROW ) {
+		while(sqlite3_step(stmt) == SQLITE_ROW){
 			ROUTEID = sqlite3_column_int(stmt, 0);
-            SRC = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)); //get col 0
-			DEST= reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)); // get col 1
-            CODE= reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3)); // get col 1
+
+            SRC = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
+			DEST = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2));
+            CODE = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3));
+
 			STOPS = sqlite3_column_int(stmt, 4);
-			
-			//std::cout<<ROUTEID<<" "<<SRC<<" "<<DEST<<" "<<CODE<<" "<<STOPS<<"\n";
-			
+
 			if(SRC == NULL){
 				srcAirport = "";
 			}
 			else{
-				srcAirport = std::string(SRC);
+				srcAirport = string(SRC);
 			}
-			
+
 			if(DEST == NULL){
 				destAirport = "";
 			}
 			else{
-				destAirport = std::string(DEST);
+				destAirport = string(DEST);
 			}
-			
+
 			if(CODE == NULL){
 				codeshare = "";
 			}
 			else{
-				codeshare = std::string(CODE);
+				codeshare = string(CODE);
 			}
-			
+
 			ID = ROUTEID;
 			stops = STOPS;
 		}
 	}
-	
+
 	sqlite3_finalize(stmt);
-	
+
 	return ID;
 }
 
-std::ostream &operator<<( std::ostream &output,const Route &R){
-    output<<R.getID()<<" "<<R.getSrc()<<" "<<R.getDest()<<" "<<R.getCodeshare()<<" "<<R.getStops()<<std::endl;
-    return output;
+ostream &operator<<(ostream &output, const Route &R){
+	output << R.getID() << " " << R.getSrc() << " " << R.getDest() << " " << R.getCodeshare() <<"  " << R.getStops() << endl;
+
+	return output;
 }
--- Code/Route.h
+++ Code/Route.h
@@ -1,50 +1,47 @@
-//
-//  Route.h
-// FlightSystem
-//
-//  Created by Darryl Murphy on 5/04/15.
-//
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#ifndef ___FlightSystem__Route__
-#define ___FlightSystem__Route__
+#ifndef ROUTE_H_
+#define ROUTE_H_
 
-#include <iostream>
 #include "sqlite3.h"
 
 class Route{
-private:
-	sqlite3* db;
-    int ID;
-    std::string srcAirport;
-    std::string destAirport;
-    std::string codeshare;
-    int stops;
-    
 public:
-    //constructors
-    Route(sqlite3*);
+	// Constructors.
+	Route(sqlite3 *);
 	Route();
-    
-    //get functions
-    int getID() const;
-    std::string getSrc() const;
-    std::string getDest() const;
-    std::string getCodeshare() const;
-    int getStops() const;
+
+	// Get functions.
+	int getID() const;
+	std::string getSrc() const;
+	std::string getDest() const;
+	std::string getCodeshare() const;
+	int getStops() const;
 	std::string getByID(int);
 	int getByAirports(std::string,std::string);
-    
-    //set Functions
-    void setID(int);
-    void setSrc(std::string);
-    void setDest(std::string);
-    void setCodeshare(std::string);
-    void setStops(int);
+
+	// Set Functions.
+	void setID(int);
+	void setSrc(std::string);
+	void setDest(std::string);
+	void setCodeshare(std::string);
+	void setStops(int);
 	int update();
-    
-    //other functions
-    friend std::ostream &operator<<( std::ostream &output,const Route &R);
-    
+
+	// Other functions.
+	friend std::ostream &operator<<(std::ostream &output, const Route &R);
+
+private:
+	sqlite3 *db;
+	int ID;
+	std::string srcAirport;
+	std::string destAirport;
+	std::string codeshare;
+	int stops;
 };
 
-#endif /* defined(___FlightSystem__Route__) */
+#endif // ROUTE_H_
