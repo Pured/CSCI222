f8ad69d1e0414c25d3faafc423559db388fb8412
Pured <krajl_kresimir@hotmail.com>
2015-05-26 23:43:39 +1000

    Restyled ServiceItem.

--- Code/ServiceItem.cpp
+++ Code/ServiceItem.cpp
@@ -1,158 +1,152 @@
-//
-//  ServiceItem.cpp
-//  222test
-//
-//  Created by Darryl Murphy on 5/04/15.
-//  Copyright (c) 2015 Darryl Murphy. All rights reserved.
-//
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#include "ServiceItem.h"
+#include <iostream>
 #include <sstream>
+#include "ServiceItem.h"
 #include "callback.h"
-#include "sqlite3.h"
 
-//constructors
-ServiceItem::ServiceItem(sqlite3* d){
-	db=d;
-    ID = -1;
-    item = "No Item";
-    cost = 0;
-    avail = "No Availability";
+using namespace std;
+
+// Constructors.
+ServiceItem::ServiceItem(sqlite3 *d){
+	db = d;
+	ID = -1;
+	item = "No Item";
+	cost = 0;
+	avail = "No Availability";
 }
 
-
-//get functions
+// Get functions.
 int ServiceItem::getID() const{
-    return ID;
+	return ID;
 }
 
-std::string ServiceItem::getItem() const{
-    return item;
+string ServiceItem::getItem() const{
+	return item;
 }
 
 float ServiceItem::getCost() const{
-    return cost;
-    
-}
-std::string ServiceItem::getAvail() const{
-    return avail;
+	return cost;
 }
 
-std::string ServiceItem::getByID(int id){
-	std::stringstream convert;
+string ServiceItem::getAvail() const{
+	return avail;
+}
+
+string ServiceItem::getByID(int id){
+	stringstream convert;
+
 	convert << id;
-	std::string convid = convert.str();
-	convert.str(std::string()); //clear ss
-	
-	std::string sqlCreate = "SELECT  * FROM SERVICEITEM WHERE ID = '" + convid +"';";
-	const char* sql = sqlCreate.c_str();
-	
+	string convid = convert.str();
+	convert.str(string()); // Clear ss.
+
+	string sqlCreate = "SELECT * FROM SERVICEITEM WHERE ID = '" + convid + "';";
+	const char *sql = sqlCreate.c_str();
+
 	sqlite3_stmt *stmt;
     int err = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-	
+
 	int SVCID = -1;
-	const char* ITEM;
-	const char* AVAIL;
 	float COST;
-	
-	if (err != SQLITE_OK) {
-        std::cout << "SELECT failed: " << sqlite3_errmsg(db) << std::endl;
-    }
-    else{
-		while (sqlite3_step(stmt) == SQLITE_ROW ) {
+	const char *ITEM, *AVAIL;
+
+	if(err != SQLITE_OK){
+		cout << "SELECT failed: " << sqlite3_errmsg(db) << endl;
+	}
+	else{
+		while(sqlite3_step(stmt) == SQLITE_ROW){
 			SVCID = sqlite3_column_int(stmt, 0);
-            ITEM = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)); //get col 0
+			ITEM = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1)); //get col 0
 			COST = sqlite3_column_double(stmt, 2); 
-            AVAIL= reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3)); // get col 1
-			
-			std::cout<<SVCID<<" "<<ITEM<<" "<<COST<<" "<<AVAIL<<"\n";
-			
+			AVAIL= reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3)); // get col 1
+
+			cout << SVCID << " " << ITEM << " " << COST << " " << AVAIL << "\n";
+
 			if(ITEM == NULL){
 				item = "";
 			}
 			else{
-				item = std::string(ITEM);
+				item = string(ITEM);
 			}
-			
+
 			if(AVAIL == NULL){
 				avail = "";
 			}
 			else{
-				avail = std::string(AVAIL);
+				avail = string(AVAIL);
 			}
-			
+
 			ID = SVCID;
 			cost = COST;
 		}
 	}
-	
+
 	sqlite3_finalize(stmt);
-	
+
 	if(ID == -1){
 		return "NOT FOUND";
 	}
-	
+
 	return "FOUND";
 }
 
-//set functions
+// Set functions.
 void ServiceItem::setID(int i){
-    ID = i;
+	ID = i;
 }
-void ServiceItem::setItem(std::string i){
-    item = i;
+void ServiceItem::setItem(string i){
+	item = i;
 }
 void ServiceItem::setCost(float i){
-    cost = i;
+	cost = i;
 }
-void ServiceItem::setAvail(std::string i){
-    avail = i;
+void ServiceItem::setAvail(string i){
+	avail = i;
 }
 
 int ServiceItem::update(){
-	
-	
 	if(ID != -1){
-		//convert any numeric attributes to string
-		std::stringstream convert;
-		convert << ID;
-		std::string convID = convert.str();
-		convert.str(std::string()); //clear ss
-	
-		convert << cost;
-		std::string convCost = convert.str();
-		convert.str(std::string()); //clear ss
-	
-	
-		std::string createSql = "UPDATE SERVICEITEM SET ID = '" + convID + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE SERVICEITEM SET ITEM = '" + item + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE SERVICEITEM SET COST = '" + convCost + "' WHERE ID = "+ convID + "; " 
-							+ 	"UPDATE SERVICEITEM SET AVAILABILITY = '" + avail + "' WHERE ID = "+ convID + "; ";
+		// Convert any numeric attributes to string.
+		stringstream convert;
 
-							
-							//std::cout<<createSql<<std::endl;
-		const char* sql = createSql.c_str();
-	
-		/* Execute SQL statement */
-   
-		char* errMsg = 0;
+		convert << ID;
+		string convID = convert.str();
+		convert.str(string()); //Clear ss.
+
+		convert << cost;
+		string convCost = convert.str();
+		convert.str(string()); //Clear ss.
+
+		string createSql = "UPDATE SERVICEITEM SET ID = '" + convID + "' WHERE ID = " + convID + ";" + "UPDATE SERVICEITEM SET ITEM = '" + item + "' WHERE ID = " + convID + ";" + "UPDATE SERVICEITEM SET COST = '" + convCost + "' WHERE ID = " + convID + ";" + "UPDATE SERVICEITEM SET AVAILABILITY = '" + avail + "' WHERE ID = " + convID + ";";
+
+		const char *sql = createSql.c_str();
+
+		// Execute SQL statement.
+		char *errMsg = 0;
 		int err = sqlite3_exec(db, sql, callback, 0, &errMsg);
-		if( err != SQLITE_OK ){
-			std::cout<<"SQL error: "<<errMsg<<std::endl;
+
+		if(err != SQLITE_OK){
+			cout << "SQL error: " << errMsg << endl;
+
 			return 1;
 		}
 	}
 	else{
-		std::cout<<"ServiceItem  not initialised in UPDATE."<<std::endl;
+		cout << "ServiceItem not initialised in UPDATE." << endl;
+
 		return 1;
 	}
 
-   return 0;
+	return 0;
 }
 
-//other functions
-std::ostream &operator<<( std::ostream &os,const ServiceItem &S){
-    os<<"Service Item ID: "<<S.getID()<<"\nItem: "<<S.getItem()<<"\nCost: "<<S.getCost()
-        <<"\nAvailability: "<<S.getAvail()<<"\n";
-    return os;
+// Other functions.
+ostream &operator<<(ostream &os, const ServiceItem &S){
+	os << "Service Item ID: " << S.getID() << "\nItem: " << S.getItem() << "\nCost: " << S.getCost() << "\nAvailability: " << S.getAvail() << "\n";
+
+	return os;
 }
--- Code/ServiceItem.h
+++ Code/ServiceItem.h
@@ -1,44 +1,42 @@
-//
-//  ServiceItem.h
-//  FlightSystem
-//
-//  Created by Darryl Murphy on 5/04/15.
-//
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#ifndef ___FlightSystem__ServiceItem__
-#define ___FlightSystem__ServiceItem__
+#ifndef SERVICEITEM_H_
+#define SERVICEITEM_H_
 
-#include <iostream>
 #include "sqlite3.h"
 
-class ServiceItem{
+class ServiceItem{    
+public:
+	// Constructors.
+	ServiceItem(sqlite3 *d);
+
+	// Get functions.
+	int getID() const;
+	std::string getItem() const;
+	float getCost() const;
+	std::string getAvail() const;
+	std::string getByID(int);
+
+	// Set functions.
+	void setID(int);
+	void setItem(std::string);
+	void setCost(float);
+	void setAvail(std::string);
+	int update();
+
+	// Other functions.
+	friend std::ostream &operator<<(std::ostream &output, const ServiceItem &S);
+
 private:
-	sqlite3* db;
+	sqlite3 *db;
     int ID;
     std::string item;
     float cost;
     std::string avail;
-    
-public:
-    //constructors
-    ServiceItem(sqlite3* d);
-    
-    //get functions
-    int getID() const;
-    std::string getItem() const;
-    float getCost() const;
-    std::string getAvail() const;
-	std::string getByID(int);
-
-    //set functions
-    void setID(int);
-    void setItem(std::string);
-    void setCost(float);
-    void setAvail(std::string);
-	int update();
-
-    //other functions
-    friend std::ostream &operator<<( std::ostream &output,const ServiceItem &S);
 };
 
-#endif /* defined(___FlightSystem__ServiceItem__) */
+#endif // SERVICEITEM_H_
