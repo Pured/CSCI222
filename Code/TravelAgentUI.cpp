34fd3466b85844ba981307d1c5732edd71c6e6bf
Pured <krajl_kresimir@hotmail.com>
2015-05-27 00:56:24 +1000

    Restyled TravelAgentUI.

--- Code/TravelAgentUI.cpp
+++ Code/TravelAgentUI.cpp
@@ -1,62 +1,67 @@
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
+#include <iostream>
 #include "TravelAgentUI.h"
 #include "BookingManagerUI.h"
 #include "CustomerProfileController.h"
 
-TravelAgentUI::TravelAgentUI(sqlite3* d) {
-    db = d;
-    userType = "TravelAgent";
-    
+using namespace std;
+
+TravelAgentUI::TravelAgentUI(sqlite3 *d){
+	db = d;
+	userType = "TravelAgent";
 }
 
-bool TravelAgentUI::run() {
-    
-    string input = "";
-	
+bool TravelAgentUI::run(){
+	string input = "";
+
 	while(input != "0"){
-        system("clear");
-        cout << "\t\t\tTRAVEL AGENT HOME" << endl;
-        cout << "please select an option: " << endl;
-        cout << "1) Book flight for a customer. " << endl;
-        cout << "2) View all bookings made." << endl;
-        cout << "3) Add new customer." << endl;
-        cout << "4) Edit own details." << endl;
-        cout << "0) Log Out." << endl;
-        cout << "Your choice: ";
-        cin >> input;
+		system("clear");
+		cout << "\t\t\tTRAVEL AGENT HOME" << endl;
+		cout << "please select an option: " << endl;
+		cout << "1) Book flight for a customer. " << endl;
+		cout << "2) View all bookings made." << endl;
+		cout << "3) Add new customer." << endl;
+		cout << "4) Edit own details." << endl;
+		cout << "0) Log Out." << endl;
+		cout << "Your choice: ";
+		cin >> input;
+
+		if(input == "1"){
+			BookingManagerUI bmUI(db);
+			input = bmUI.run();
+			//bookFlights();
+		}
+		else if(input == "2"){
 			
-        if(input == "1"){
-            BookingManagerUI bmUI(db);
-            input = bmUI.run();
-            //bookFlights();
-        }
-        else if(input == "2"){
-            bookingsMade();
-        }
-        else if(input == "3"){
-            CustomerProfileController cpc(db);
-            cpc.createCustomer();
-            //addCustomer();
-        }
-        else if(input == "4"){
-            editDetails();
-        }
+		}
+		else if(input == "3"){
+			CustomerProfileController cpc(db);
+			cpc.createCustomer();
+			//addCustomer();
+		}
+		else if(input == "4"){
+			editDetails();
+		}
 	}
-    system("clear");
-    return 1;
+
+	system("clear");
+
+	return 1;
 }
 
 void TravelAgentUI::bookingsMade(){
-    
-    
+	cout << "NOT IMPLEMENTED.\n";
 }
 
 void TravelAgentUI::addCustomer(){
-    
-    
+	cout << "NOT IMPLEMENTED.\n";
 }
 
 void TravelAgentUI::editDetails(){
-    
-    
+	cout << "NOT IMPLEMENTED.\n";
 }
--- Code/TravelAgentUI.h
+++ Code/TravelAgentUI.h
@@ -1,27 +1,28 @@
+/*=============================================================
+| Modified by: kb100
+| Version: 1.01
+| Modification: Restyled the code.
+|==============================================================*/
 
-#ifndef TravelAgentUI_H
-#define TravelAgentUI_H
+#ifndef TravelAgentUI_H_
+#define TravelAgentUI_H_
 
-#include <iostream>
 #include <string>
 #include "sqlite3.h"
 
-using namespace std;
-
-
-class TravelAgentUI
-{
+class TravelAgentUI{
 public:
-    TravelAgentUI(sqlite3*);
+    TravelAgentUI(sqlite3 *);
+
     bool run();
     //void bookFlights();
     void bookingsMade();
     void addCustomer();
     void editDetails();
-protected:
+
 private:
-    sqlite3* db;
+    sqlite3 *db;
     std::string userType;
 };
 
-#endif /* defined(____TravelAgentUI__) */
+#endif // TravelAgentUI_H_
