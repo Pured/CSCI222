38fc2cce98359a63be08d3d056817c0253ec0b1d
Pured <krajl_kresimir@hotmail.com>
2015-05-27 20:04:09 +1000

    Fixed BookingManagerUI + ProfileManagerUI.
    They now follow the client's requirements correctly.

--- Code/ProfileManagerUI.cpp
+++ Code/ProfileManagerUI.cpp
@@ -1,12 +1,13 @@
 /*=============================================================
 | Modified by: kb100
-| Version: 1.03
-| Modification: Restyled the code.
+| Version: 1.04
+| Modification: Added customerAccessMenu().
 |==============================================================*/
 
 #include <iostream>
 #include "ProfileManagerUI.h"
 #include "ProfileManagerController.h"
+#include "CustomerProfileController.h"
 
 using namespace std;
 
@@ -21,10 +22,10 @@ bool ProfileManagerUI::run(){
 
 	cout << endl;
 
-	while (input != "0"){
+	while(input != "0"){
 		cout << "\t\t\tPROFILE MANAGER HOME\n\n";
 		cout << "Please choose an option:\n\n";
-		cout << "1) Search for a customer.\n";
+		cout << "1) Access customer.\n";
 		cout << "2) Access profile reports.\n";
 		cout << "0) Log out.\n\n";
 		cout << "Your choice: ";
@@ -32,13 +33,13 @@ bool ProfileManagerUI::run(){
 
 		cout << endl;
 
-		if (input == "1"){
-			pmc.findCustomer();
+		if(input == "1"){
+			customerAccessMenu();
 		}
-		else if (input == "2"){
+		else if(input == "2"){
 			pmc.profileReport();
 		}
-		else if (input == "0"){
+		else if(input == "0"){
 			return 1;
 		}
 		else{
@@ -47,4 +48,44 @@ bool ProfileManagerUI::run(){
 	}
 
 	return 0;
+}
+
+void ProfileManagerUI::customerAccessMenu(){
+	CustomerProfileController cpc(db);
+	string input = "-1";
+
+	cout << endl;
+
+	while(input != "0"){
+		cout << "Customer Access Menu:\n";
+		cout << "Please choose an option:\n\n";
+		cout << "1) Search for a customer.\n";
+		cout << "2) Create a new customer.\n";
+		cout << "3) Edit a customer.\n";
+		cout << "4) Delete a customer.\n";
+		cout << "0) Return to main menu.\n\n";
+		cout << "Your choice: ";
+		cin >> input;
+
+		cout << endl;
+
+		if(input == "1"){
+			cpc.findCustomer();
+		}
+		else if(input == "2"){
+			cpc.createCustomer();
+		}
+		else if(input == "3"){
+			cpc.editCustomer(userType,"");
+		}
+		else if(input == "4"){
+			cpc.deleteCustomer();
+		}
+		else if(input == "0"){
+			//...
+		}
+		else{
+			cout << "Invalid input.\n\n";
+		}
+	}
 }
